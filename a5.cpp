397c0d035a9e6c08555a9f762f6782f622b57419
ellen <twk45vymwk@privaterelay.appleid.com>
2026-04-07 12:30:41 -0700

    report

--- a5.cpp
+++ a5.cpp
@@ -1,31 +1,5 @@
 // a5.cpp
 
-/////////////////////////////////////////////////////////////////////////
-//
-// Student Info
-// ------------
-//
-// Name         : Hoang Mai Tran
-// Student ID # : 301644559
-// SFU Email    : hmt9@sfu.ca
-// Platform     : macOS
-// Editor/IDE   : VSCode
-// Known Bugs   : 
-// Missing      : 
-// Challenges   : 
-// Help         : 
-// ------------------------
-//
-// All the code and comments below are our own original work. For any non-
-// original work, we have provided citations above and in the comments with
-// enough detail so that someone can see the exact source and extent of the
-// borrowed work.
-//
-// In addition, we have not shared this work with anyone else, and we have not
-// seen solutions from other students, tutors, websites, books, etc.
-//
-/////////////////////////////////////////////////////////////////////////
-
 #include <iostream>
 #include <string>
 #include <vector>
