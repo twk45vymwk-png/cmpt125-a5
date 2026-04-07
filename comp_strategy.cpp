34fa8fe95af01dc7abff387b26def7c076760f07
ellen <twk45vymwk@privaterelay.appleid.com>
2026-04-06 17:03:08 -0700

    comp_strategy.cpp

--- comp_strategy.cpp
+++ comp_strategy.cpp
@@ -8,6 +8,7 @@ using namespace std;
 const int ROWS = 6;
 const int COLS = 7;
 const int WIN_LEN = 4;
+const int MAX_DEPTH = 7;
 
 enum class Cell { Empty, player1, player2, anvil1, anvil2}; // Cell type
 
@@ -180,6 +181,130 @@ int minimax(Board board,
     int next_player = opponent (current_player);
 
     if (maximizing){
-        int best
+        int best = numeric_limits<int>::min();
+        for (int c = 0; c < COLS; c++){
+            if (col_full(board,c)){
+                continue;
+            }
+            Board next = board;
+            drop (next, c, current_player);
+            int val = minimax (next, depth - 1, alpha, beta, false, next_player, comp_player);
+            best = max(best, val);
+            alpha = max(alpha, val);
+            if (alpha >= beta){
+                break;
+            }
+        }
+        return best;
     }
+
+    else{
+        int best = numeric_limits <int>::max();
+        for (int c = 0; c < COLS; c++){
+            if (col_full(board, c)){
+                continue;
+            }
+            Board next = board;
+            drop (next, c, current_player);
+            int val = minimax (next, depth - 1, alpha, beta, true, next_player, comp_player);
+            best = min(best, val);
+            beta = min(alpha, val);
+            if (alpha >= beta){
+                break;
+            }
+        }
+        return best;
+    }
+}
+
+// Choose the best column for the computer to play a normal piece
+int best_computer_move (const Board& board, int comp_player){
+    int best_score = numeric_limits<int>::min();
+    int best_col = -1;
+
+    //TRy columns in order from centre outward
+    vector <int> order;
+    int mid = COLS/2;  // Start with the middle column because it's usually the strongest
+    order.push_back(mid);
+    for (int off = 1; off <= mid; off++){
+        if (mid + off < COLS){
+            order.push_back(mid + off);
+        }
+        if (mid - off >= 0){
+            order.push_back(mid - off);
+        }
+    }
+
+    for (int c : order){
+        if (col_full(board,c)){
+            continue;
+        }
+        Board next = board;
+        drop (next, c , comp_player);
+        int score = minimax (next, MAX_DEPTH - 1,
+                             numeric_limits <int>::min(),
+                             numeric_limits <int>::max(),
+                             false,
+                             opponent(comp_player),
+                             comp_player); // Ask minimax
+        if (score > best_score){
+            best_score = score;
+            best_col = c;
+        }
+    }
+    return best_col;
+}
+
+// Decide whether the computer should use its anvil this turn
+int best_computer_anvil(const Board& board, int comp_player, bool anvil_available){
+    if (!anvil_available){
+        return -1; // Don't use anvil
+    }
+
+    int opp = opponent(comp_player);
+
+    // (1) Check if dropping anvil wins immediately for computer
+    for (int c = 0; c<COLS; c++){
+        if (col_full(board,c)){
+            continue;
+        }
+        Board next = board;
+        drop_anvil (next, c, comp_player);
+        if (win(next, comp_player)){
+            return c;
+        }
+    }
+
+    // (2) Check if dropping anvil blocks an opponent win
+    for (int c = 0; c<COLS; c++){
+        if (col_full(board,c)){
+            continue;
+        }
+        Board test = board;
+        if (drop(test, c, opp) && win(test, opp)){
+            Board next = board;
+            drop_anvil(next, c, comp_player);
+            if (!win(next, opp)){
+                return c;
+            }
+        }
+    }
+
+    // (3) Use anvil to break a long opponent chain a column
+    for (int c = 0; c<COLS; c++){
+        if (col_full(board,c)){
+            continue;
+        }
+        int opp_count = 0;
+        for (int r = 0; r < ROWS; r++){
+            if (is_player(board[r][c], opp)){
+                opp_count++;
+            }
+        }
+        if (opp_count >= 3){
+            return c;
+        }
+    }
+
+    return -1; // don't use anvil
 }
